debef4cd7c8a920832cb54bf1c35cea305d31562
LeTienDung <letiendungbk@yahoo.com.vn>
2013-06-01 23:18:49 +0700

    Initial Commit

---
+++ PhotoAndMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoAndMe
+//
+//  Created by LeTienDung on 6/1/13.
+//  Copyright (c) 2013 LeTienDung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PhotoAndMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoAndMe
+//
+//  Created by LeTienDung on 6/1/13.
+//  Copyright (c) 2013 LeTienDung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
